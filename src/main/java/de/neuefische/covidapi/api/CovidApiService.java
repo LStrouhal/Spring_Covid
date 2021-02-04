fd6b451605fec75d48b0e2bbf27ce7bc90eff1ba
ArmbrustR <rafael.armbrust@gmail.com>
2021-02-04 15:30:50 +0100

    RafaelEdit

--- src/main/java/de/neuefische/covidapi/api/CovidApiService.java
+++ src/main/java/de/neuefische/covidapi/api/CovidApiService.java
@@ -2,8 +2,10 @@ package de.neuefische.covidapi.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
+@Repository
 public class CovidApiService {
 
     private final RestTemplate restTemplate;
--- src/main/java/de/neuefische/covidapi/service/CovidService.java
+++ src/main/java/de/neuefische/covidapi/service/CovidService.java
@@ -1,12 +1,16 @@
 package de.neuefische.covidapi.service;
 
 
+import de.neuefische.covidapi.api.CovidApiCountryStatusData;
 import de.neuefische.covidapi.api.CovidApiService;
+import de.neuefische.covidapi.model.CovidCases;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.Optional;
 
+@Service
 public class CovidService {
 
     private final CovidApiService covidApiService;
@@ -15,4 +19,16 @@ public class CovidService {
     public CovidService(CovidApiService covidApiService) {
         this.covidApiService = covidApiService;
     }
+
+    public Optional<CovidCases> getActiveCases() {
+        CovidApiCountryStatusData[] cases = covidApiService.getCases();
+        for (CovidApiCountryStatusData aCase : cases) {
+            if (aCase.getProvince().equals("Hamburg")) {
+                return Optional.of(new CovidCases(aCase.getProvince(), aCase.getActive(), aCase.getDate()));
+            }
+        }
+        return Optional.empty();
+
+    }
+
 }
